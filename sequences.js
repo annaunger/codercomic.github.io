b1e5368ebbcfa314a2db249e50c6018082ae9fe8
CoderComic <YEMIBYTES@HYPERBEAST-MAC.local>
2016-09-15 16:39:48 +0200

    GraPH

--- sequences.js
+++ sequences.js
@@ -2,12 +2,12 @@ var width = 750;
 var height = 600;
 var radius = Math.min(width, height) / 2;
 
-// Breadcrumb dimensions: width, height, spacing, width of tip/tail.
+
 var b = {
   w: 75, h: 30, s: 3, t: 10
 };
 
-// Mapping of step names to colors.
+
 var colors = {
   "home": "#5687d1",
   "product": "#7b615c",
@@ -17,7 +17,7 @@ var colors = {
   "end": "#bbbbbb"
 };
 
-// Total size of all segments; we set this later, after loading the data.
+
 var totalSize = 0; 
 
 var vis = d3.select("#chart").append("svg:svg")
@@ -37,15 +37,14 @@ var arc = d3.svg.arc()
     .innerRadius(function(d) { return Math.sqrt(d.y); })
     .outerRadius(function(d) { return Math.sqrt(d.y + d.dy); });
 
-// Use d3.text and d3.csv.parseRows so that we do not need to have a header
-// row, and can receive the csv as an array of arrays.
+
 d3.text("visit-sequences.csv", function(text) {
   var csv = d3.csv.parseRows(text);
   var json = buildHierarchy(csv);
   createVisualization(json);
 });
 
-// Main function to draw and set up the visualization, once we have the data.
+
 function createVisualization(json) {
 
   // Basic setup of page elements.
@@ -296,4 +295,4 @@ function buildHierarchy(csv) {
     }
   }
   return root;
-}
+};
